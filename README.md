2db123c932ea866f9c2c4e41155ac7a01f45fed8
hahv <hoangvanhauit@gmail.com>
2022-06-13 09:23:24 +0900

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # HOW TO THESE YOLOV5 CODE WITH MOBILITY DATASET
 
+## Origin work YOLOV5: <https://github.com/ultralytics/yolov5>
+
+
 + **Step 1**: Install requirement described in `requirements.txt` file
 
 + **Step 2**: In the folder `data`, create a folder named `mobility`. Download and copy the train/valid/test data into that folder.
@@ -7,11 +10,11 @@
   + For mobility dataset (4 classes of group 2: *Micro Car, SUV, Bus, Van*), data can be downloaded from this [link](https://drive.google.com/drive/folders/12EiV92VHRoM8R9N9SL-3okPG2TU1fl5b?usp=sharing)
   + For both cases, copy file `mobility.yaml` (in downloaded data) and put it into `data` folder
 
-+ **Step 3**: For training, run the code in the file `zrun.bat` with command prompt (terminal in macOS) - edit the batch, epoch, etc. (the argument --img 640 is fixed for pretrained model yolov5l)
++ **Step 3**: For training, run the code in the file `zrun.bat` with command prompt (terminal in macOS) - edit the batch, epoch, etc. (the argument --img 640 is fixed for pretrained model yolov5l). You can change the **[data augmentation](https://blog.roboflow.com/yolov5-improvements-and-evaluation/) configuration** in file `data/hyps/hyp.scratch-low.yaml`
 
 + **Step 4**: For run detection, run the code in the file `zdetect.bat` with command prompt (terminal in macOS) - edit the --conf threshold and the path for image folder.
-You can change the **data augmentation configuration** in file `data/hyps/hyp.scratch-low.yaml`
+
   
 + **Step 5**: Check the results (for training or detect) inside folder `runs`
 
-https://blog.roboflow.com/yolov5-improvements-and-evaluation/
+
--- data/test_data.py
+++ data/test_data.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from colorama import Fore, Back, Style, init
 import math
 import random
+import json
 
 init(autoreset=True)
 
@@ -16,30 +17,14 @@ def hLine():
     print(Fore.RED + '*' * 50)
 
 
-def main():
-
-    test_folder = './mobility4'
-
-    num_class = int(re.findall(r'\d+', test_folder)[0])
-
-    test_img_per_class = 100
-    train_img_per_class = int(400 * 0.8)
-    valid_img_per_class = int(400 * 0.2)
-
-    require_dict = {'train': train_img_per_class, 'valid': valid_img_per_class, 'test': test_img_per_class}
-
-    folders = filesys.list_dirs(test_folder)
-
-    actual_info = {}
-
+def del_invalid_pair_img_lb(test_folder, ignore_dirs):
     # delete invalid image/label pairs
+    folders = filesys.list_dirs(test_folder)
     for folder in folders:
-        if 'new' in folder:
+        if folder in ignore_dirs:
             continue
         img_files = filesys.list_files(f'{test_folder}/{folder}/images')
         files = filesys.list_files(f'{test_folder}/{folder}/labels')
-        actual_info[folder + '_cat'] = {}
-        # print(files[0])
 
         # delete empty files
         for f in files:
@@ -60,36 +45,48 @@ def main():
                 print(f'{path_txt} not exists')
                 filesys.delete_file(path)
 
+
+def count_img_by_category(test_folder, ignore_dirs):
+    count_info = {}
+    folders = filesys.list_dirs(test_folder)
+
     # count by cate
     for folder in folders:
-        if 'new' in folder:
+        if folder in ignore_dirs:
             continue
         files = filesys.list_files(f'{test_folder}/{folder}/labels')
-        actual_info[folder + '_cat'] = {}
+        count_info[folder] = {}
         for f in files:
             path = f'{test_folder}/{folder}/labels/{f}'
             lines = textfile.read_line_by_line(path)
             cate = int(lines[0].split(' ')[0])
 
-            if cate in actual_info[folder + '_cat']:
-                actual_info[folder + '_cat'][cate]['count'] += 1
-                actual_info[folder + '_cat'][cate]['files'] += [f]
+            if cate in count_info[folder]:
+                count_info[folder][cate]['count'] += 1
+                count_info[folder][cate]['files'] += [f]
             else:
-                actual_info[folder + '_cat'][cate] = {'count': 1, 'files': [f]}
+                count_info[folder][cate] = {'count': 1, 'files': [f]}
+    return count_info
 
-    print(actual_info)
-    # duplicate
 
+def duplicate_to_get_required_size(test_folder, require_dict, count_info, ignore_dirs):
+    folders = filesys.list_dirs(test_folder)
     for folder in folders:
-        if 'new' in folder:
+        if folder in ignore_dirs:
             continue
-        folder_dict = actual_info[folder + '_cat']
+
+        folder_dict = count_info[folder]
         for cat in folder_dict:
             cat_dict = folder_dict[cat]
             no_cate = cat_dict['count']
             files = cat_dict['files']
             lack_no = require_dict[folder] - no_cate
-            print(folder, cat, lack_no)
+            print(folder, cat, len(files), Fore.GREEN + str(require_dict[folder]), Fore.RED + str(lack_no))
+
+            if(lack_no <= 0):
+                continue
+            if(lack_no > len(files)):
+                lack_no = len(files)
 
             to_copy_files = random.sample(files, lack_no)
             for cfile in to_copy_files:
@@ -104,6 +101,49 @@ def main():
 
                 filesys.copy_file(lb_file, dest_folder_lb)
                 filesys.copy_file(img_file, dest_folder_img)
+
+
+def split_train_val(test_folder, count_info, train_img_per_class, valid_img_per_class):
+    train_dict = count_info['train']
+    for cate in train_dict:
+        cate_files = train_dict[cate]['files']
+        if len(cate_files) == train_img_per_class:
+            continue
+        
+        valid_files = random.sample(cate_files, valid_img_per_class)
+        for lb_file in valid_files:
+            k = lb_file.rfind(".")
+            img_name = lb_file[:k] + ".jpg"
+            img_file_path = f'{test_folder}/train/images/{img_name}'
+            lb_file_path = f'{test_folder}/train/labels/{lb_file}'
+            
+            filesys.move_dir_or_file(img_file_path, f'{test_folder}/valid/images')
+            filesys.move_dir_or_file(lb_file_path, f'{test_folder}/valid/labels')
+
+def main():
+
+    test_folder = './mobility15'
+
+    test_img_per_class = 100
+    train_img_per_class = int(400 * 0.8)
+    valid_img_per_class = int(400 * 0.2)
+
+    # train_img_per_class = 400
+    # valid_img_per_class = 0
+
+    require_dict = {'train': train_img_per_class, 'valid': valid_img_per_class, 'test': test_img_per_class}
+
+    del_invalid_pair_img_lb(test_folder, ignore_dirs=['new'])
+    count_info = count_img_by_category(test_folder, ignore_dirs=['new'])
+
+    print(json.dumps(require_dict, sort_keys=True, indent=4))
+    hLine()
+    print(json.dumps(count_info, sort_keys=True, indent=4))
+    hLine()
+    duplicate_to_get_required_size(test_folder, require_dict, count_info, ignore_dirs=['new'])
+    print(Fore.MAGENTA + str(require_dict))
+
+    split_train_val(test_folder, count_info, train_img_per_class, valid_img_per_class)
 
 
 if __name__ == "__main__":
